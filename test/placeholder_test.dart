8f9553cddccf2c0bae74dbdc49be6aba0ec4ba2e
felipe <felipebrostolinribeiro@gmail.com>
2024-08-20 11:46:56 -0300

    Added some dependencies and a placeholder test

--- test/placeholder_test.dart
+++ test/placeholder_test.dart
@@ -1,3 +1,10 @@
 // TODO: remove this file after creating real tests. This is here to please the github workflow, for now.
 
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // remove this test after creating real tests
+  test('placeholder', () {
+    expect(1, 1);
+  });
+}
