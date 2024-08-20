178f364555074eaacd9ad9891acb2d5448998f75
felipe <felipebrostolinribeiro@gmail.com>
2024-08-20 11:13:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Easy Report
+
+This project has the objective to be a simple report template editor, that uses JSON as variables to build dynamic reports. 
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Easy Report',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Easy Report"),
+        ),
+      ),
+    );
+  }
+}
